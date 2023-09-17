1afeb56cd0d34243b7b9e6fc94f49b07322fc6cd
Tanisha Sharma <92216346+Tanishasharma11@users.noreply.github.com>
2023-09-17 22:47:12 +0530

    02-nbody.cu

--- 02-nbody.cu
+++ 02-nbody.cu
@@ -1,24 +1,30 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "timer.h"
 #include "files.h"
 
 #define SOFTENING 1e-9f
 
-typedef struct { float x, y, z, vx, vy, vz; } Body;
+// Structure to represent a body
+typedef struct {
+    float x, y, z, vx, vy, vz;
+} Body;
 
-__global__ void bodyForceKernel(const Body* __restrict__ p, Body* __restrict__ newp, float dt, int n) {
+// CUDA kernel for calculating gravitational forces using a grid-stride loop
+__global__ void bodyForce(Body *p, float dt, int n) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
 
-    if (i < n) {
+    for (; i < n; i += stride) {
         float Fx = 0.0f, Fy = 0.0f, Fz = 0.0f;
+        float px = p[i].x, py = p[i].y, pz = p[i].z;
 
         for (int j = 0; j < n; j++) {
-            float dx = p[j].x - p[i].x;
-            float dy = p[j].y - p[i].y;
-            float dz = p[j].z - p[i].z;
-            float distSqr = dx*dx + dy*dy + dz*dz + SOFTENING;
+            float dx = p[j].x - px;
+            float dy = p[j].y - py;
+            float dz = p[j].z - pz;
+            float distSqr = dx * dx + dy * dy + dz * dz + SOFTENING;
             float invDist = rsqrtf(distSqr);
             float invDist3 = invDist * invDist * invDist;
 
@@ -27,13 +33,13 @@ __global__ void bodyForceKernel(const Body* __restrict__ p, Body* __restrict__ n
             Fz += dz * invDist3;
         }
 
-        newp[i].vx += dt * Fx;
-        newp[i].vy += dt * Fy;
-        newp[i].vz += dt * Fz;
+        p[i].vx += dt * Fx;
+        p[i].vy += dt * Fy;
+        p[i].vz += dt * Fz;
     }
 }
 
-int main(const int argc, const char** argv) {
+int main(int argc, const char** argv) {
     int nBodies = 2 << 11;
     if (argc > 1) nBodies = 2 << atoi(argv[1]);
 
@@ -43,7 +49,7 @@ int main(const int argc, const char** argv) {
     if (nBodies == 2 << 11) {
         initialized_values = "09-nbody/files/initialized_4096";
         solution_values = "09-nbody/files/solution_4096";
-    } else { // nBodies == 2<<15
+    } else { 
         initialized_values = "09-nbody/files/initialized_65536";
         solution_values = "09-nbody/files/solution_65536";
     }
@@ -51,69 +57,64 @@ int main(const int argc, const char** argv) {
     if (argc > 2) initialized_values = argv[2];
     if (argc > 3) solution_values = argv[3];
 
-    const float dt = 0.01f; // Time step
-    const int nIters = 10;  // Simulation iterations
+    const float dt = 0.01f;
+    const int nIters = 10;
 
     int bytes = nBodies * sizeof(Body);
-    float* buf;
-
-    buf = (float*)malloc(bytes);
-
+    float* buf = (float*)malloc(bytes);
     Body* p = (Body*)buf;
-    Body* newp = (Body*)malloc(bytes);
 
     read_values_from_file(initialized_values, buf, bytes);
 
     double totalTime = 0.0;
 
-    // Allocate GPU memory for bodies
+    // Allocate GPU memory
     Body* d_p;
-    Body* d_newp;
     cudaMalloc((void**)&d_p, bytes);
-    cudaMalloc((void**)&d_newp, bytes);
 
-    // Transfer data from CPU to GPU
-    cudaMemcpy(d_p, p, bytes, cudaMemcpyHostToDevice);
-
-    // Specify thread block size and grid size for CUDA kernels
-    int blockSize = 256;
-    int numBlocks = (nBodies + blockSize - 1) / blockSize;
+    // Get CUDA device properties to determine the number of SMs
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, 0); // Assumes GPU device 0
 
     for (int iter = 0; iter < nIters; iter++) {
         StartTimer();
 
-        // Launch bodyForce kernel
-        bodyForceKernel<<<numBlocks, blockSize>>>(d_p, d_newp, dt, nBodies);
+        // Copy data from CPU to GPU
+        cudaMemcpy(d_p, p, bytes, cudaMemcpyHostToDevice);
 
-        // Synchronize to ensure completion of bodyForce
+        int threadsPerBlock = 256;
+        int blocksPerSM = prop.maxThreadsPerMultiProcessor / threadsPerBlock;
+        int numberOfBlocks = prop.multiProcessorCount * blocksPerSM;
+
+        // Launch the bodyForce kernel with a grid-stride loop
+        bodyForce<<<numberOfBlocks, threadsPerBlock>>>(d_p, dt, nBodies);
+
+        // Wait for the kernel to finish
         cudaDeviceSynchronize();
+
+        // Copy data from GPU to CPU
+        cudaMemcpy(p, d_p, bytes, cudaMemcpyDeviceToHost);
+
+        // Integrate positions on CPU
+        for (int i = 0; i < nBodies; i++) {
+            p[i].x += p[i].vx * dt;
+            p[i].y += p[i].vy * dt;
+            p[i].z += p[i].vz * dt;
+        }
 
         const double tElapsed = GetTimer() / 1000.0;
         totalTime += tElapsed;
-
-        // Swap the pointers to update positions
-        Body* temp = d_p;
-        d_p = d_newp;
-        d_newp = temp;
     }
-
-    // Transfer data back from GPU to CPU
-    cudaMemcpy(p, d_p, bytes, cudaMemcpyDeviceToHost);
-
-    // Free GPU memory
-    cudaFree(d_p);
-    cudaFree(d_newp);
 
     double avgTime = totalTime / (double)(nIters);
     float billionsOfOpsPerSecond = 1e-9 * nBodies * nBodies / avgTime;
     write_values_to_file(solution_values, buf, bytes);
 
-    // Print performance metric
     printf("%0.3f Billion Interactions / second\n", billionsOfOpsPerSecond);
 
-    // Free CPU memory
+    // Free GPU memory
+    cudaFree(d_p);
     free(buf);
-    free(newp);
 
     return 0;
 }
